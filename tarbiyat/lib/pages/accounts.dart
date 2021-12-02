929d688378dede169100afaf828497742425b69c
alimadras <alimadras@msbinstitute.com>
2021-12-02 09:37:48 +0530

    git git

--- tarbiyat/lib/pages/accounts.dart
+++ tarbiyat/lib/pages/accounts.dart
@@ -13,33 +13,39 @@ class accounts extends StatefulWidget {
 class _accountsState extends State<accounts> {
   Map udata = {};
 
-    //check login
-    Future<void> _checkloc() async {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String itsid = prefs.getString('itsid') ?? '0';
-      if (itsid != '0') {
-        udata = jsonDecode(itsid);
-        print(udata['itsid']);
-      } else {
-        Navigator.pushReplacementNamed(context, '/login');
-      }
+  //check login
+  Future<void> _checkloc() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String itsid = prefs.getString('itsid') ?? '0';
+    if (itsid != '0') {
+      udata = jsonDecode(itsid);
+      print(udata['itsid']);
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
     }
-    //get all ids
-    Map itsidsl = {};
-    Future<void> _getitsids() async{
-      String itsids ='[';
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-        itsids += prefs.containsKey('itsid') ? prefs.getString('itsid')!+',':'';
-        itsids += prefs.containsKey('itsid2') ? prefs.getString('itsid2')!+',':'';
-        itsids += prefs.containsKey('itsid3') ? prefs.getString('itsid3')!+',':'';
-        itsids += prefs.containsKey('itsid4') ? prefs.getString('itsid4')!+',':'';
-        itsids += prefs.containsKey('itsid5') ? prefs.getString('itsid5')!+',':'';
-        itsidsl = jsonDecode(itsids);
-    }
+  }
+
+  //get all ids
+  Map itsidsl = {};
+  Future<void> _getitsids() async {
+    String itsids = '[';
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    itsids += prefs.containsKey('itsid') ? prefs.getString('itsid')! + ',' : '';
+    itsids +=
+        prefs.containsKey('itsid2') ? prefs.getString('itsid2')! + ',' : '';
+    itsids +=
+        prefs.containsKey('itsid3') ? prefs.getString('itsid3')! + ',' : '';
+    itsids +=
+        prefs.containsKey('itsid4') ? prefs.getString('itsid4')! + ',' : '';
+    itsids +=
+        prefs.containsKey('itsid5') ? prefs.getString('itsid5')! + ',' : '';
+    itsids += ']';
+    itsidsl = jsonDecode(itsids);
+  }
+
   @override
   Widget build(BuildContext context) {
     _checkloc();
-    
 
     return Scaffold(
       appBar: AppBar(
@@ -49,13 +55,17 @@ class _accountsState extends State<accounts> {
       drawer: NavDrawer(),
       body: ListView(
         children: [
-          Card(
-            margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-            child:Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Text(,
-                style: TextStyle(fontSize: 18.0, color: Colors.grey[600])),
-            ),
-          ),
+          for (var i in itsidsl.keys)
+            Card(
+              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(itsidsl[i].itsid,
+                        style:
+                            TextStyle(fontSize: 18.0, color: Colors.grey[600])),
+                  ]),
+            )
         ],
       ),
     );
